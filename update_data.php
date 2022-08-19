2a5fcf80555777050faf0c94eac5ba6ff7680ef4
EdgarG15 <edgarjhdez1501@gmail.com>
2022-08-19 15:33:15 -0600

    first commit

---
+++ update_data.php
@@ -0,0 +1,75 @@
+<?php
+
+include("dbconnection.php");
+$con=dbconnection();
+
+if(isset($_POST["id"]))
+{
+    $id=$_POST["id"];
+}
+else return;
+
+if(isset($_POST["name"]))
+{
+    $name=$_POST["name"];
+}
+else return;
+
+if(isset($_POST["email"]))
+{
+    $email=$_POST["email"];
+}
+else return; 
+
+if(isset($_POST["password"]))
+{
+    $password=$_POST["password"];
+}
+else return; 
+
+$query="UPDATE `user_table` SET `uname`='$name', `uemail`='$email', `upassword`='$password' WHERE uid='$id'";
+$exe=mysqli_query($con,$query);
+$arr=[];
+if($exe){
+    $arr["success"]="true";
+}
+else{
+    $arr["success"]="false";
+}
+print(json_encode($arr));
+
+
+// include("dbconnection.php");
+// $con=dbconnection();
+
+// if(isset($_POST["name"]))
+// {
+//     $name=$_POST["name"];
+// }
+// else return;
+
+// if(isset($_POST["email"]))
+// {
+//     $email=$_POST["email"];
+// }
+// else return; 
+
+// if(isset($_POST["password"]))
+// {
+//     $password=$_POST["password"];
+// }
+// else return; 
+
+// $query="UPDATE `user_table` SET `uname`='$name',`upassword`='$password' WHERE `uemail`='$email'";
+// $exe=mysqli_query($con,$query);
+// $arr=[];
+// if($exe){
+//     $arr["success"]="true";
+// }
+// else{
+//     $arr["success"]="false";
+// }
+// print(json_encode($arr));
+
+
+?>
